ff8b6ec664fd529dce9ec07b0a9c8fc906626a32
alexbrunoliveira <alexbrunoliveira@gmail.com>
2022-04-10 23:32:28 -0300

    AULA - Lendo Excel XLS com Apache Poi e montando lista

--- arquivos/src/arquivos/ApachePoi.java
+++ arquivos/src/arquivos/ApachePoi.java
@@ -11,72 +11,74 @@ import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 
 public class ApachePoi {
-	
+
 	public static void main(String[] args) throws Exception {
-		
-		File file = new File("C:\\Users\\alex_\\git\\repository3\\arquivos\\\\arquivo_excel.xls");
-		
+
+		File file = new File("C:\\Users\\alex_\\git\\repository3\\arquivos\\arquivo_excel.xls");
+
 		if (!file.exists()) {
 			file.createNewFile();
-			 
 		}
-		
+
 		Pessoa pessoa1 = new Pessoa();
 		pessoa1.setEmail("pessoa1@gmail.com");
 		pessoa1.setIdade(50);
-		pessoa1.setNome("Alex Bruno");
-		
+		pessoa1.setNome("Alex egidio");
+
 		Pessoa pessoa2 = new Pessoa();
 		pessoa2.setEmail("pessoa2@gmail.com");
 		pessoa2.setIdade(25);
-		pessoa2.setNome("Marcos Tadeu");
-		
+		pessoa2.setNome("Marcos tadeu");
+
 		Pessoa pessoa3 = new Pessoa();
 		pessoa3.setEmail("pessoa3@gmail.com");
 		pessoa3.setIdade(40);
-		pessoa3.setNome("Maria Jesus");
+		pessoa3.setNome("Maria jesus");
+		
 		
 		Pessoa pessoa4 = new Pessoa();
 		pessoa4.setEmail("pessoa4@gmail.com");
 		pessoa4.setIdade(45);
-		pessoa4.setNome("João Carlos");
-		
-		/*Pode vir do banco de dados ou qualquer fonte de dados*/
-		
+		pessoa4.setNome("Paulo pedro");
+
+		/* Pode vir do banco de dados ou qualquer fonte de dados */
 		List<Pessoa> pessoas = new ArrayList<Pessoa>();
+		
 		pessoas.add(pessoa1);
 		pessoas.add(pessoa2);
 		pessoas.add(pessoa3);
 		pessoas.add(pessoa4);
-
 		
-		HSSFWorkbook hssfWorkbook = new HSSFWorkbook(); // Usado para escrever na planilha
-		HSSFSheet linhasPessoa = hssfWorkbook.createSheet("Planilha de Pessoas Jdev Treinamento"); // Criar planilha
+		HSSFWorkbook hssfWorkbook = new HSSFWorkbook(); /*Vai ser usado para escrever a planilha*/
+		HSSFSheet linhasPessoa = hssfWorkbook.createSheet("Planilha de pessoas Jdev Treinamento"); /*Criar a planilha*/
 		
-		int numeroLinha = 0 ; // Cria uma linha para cada pessoa
+		int numeroLinha = 0;
 		
 		for (Pessoa p : pessoas) {
-			Row linha = linhasPessoa.createRow(numeroLinha ++); // Criando linha na tabela
+			Row linha = linhasPessoa.createRow(numeroLinha ++ ); /*Criando a linha na planilha*/
 			
-			int celula  = 0;
+			int celula = 0;
 			
-			Cell celNome = linha.createCell(celula ++) ; // Celula 1
+			Cell celNome = linha.createCell(celula ++);/*Celular 1*/
 			celNome.setCellValue(p.getNome());
 			
-			Cell celemail = linha.createCell(celula ++) ; // Celula 2
+			
+			Cell celemail = linha.createCell(celula ++); /*Celular 2 */
 			celemail.setCellValue(p.getEmail());
 			
-			Cell celIdade = linha.createCell(celula ++) ; // Celula 3
+			
+			Cell celIdade = linha.createCell(celula ++); /*Celular 3*/
 			celIdade.setCellValue(p.getIdade());
-			
-		} /*Termino de montra planilha*/
-			
+		
+		}/*Termino de montrar a planilha*/
+		
 		FileOutputStream saida = new FileOutputStream(file);
-		hssfWorkbook.write(saida); /*Escreve planilha em arquivo*/
+		hssfWorkbook.write(saida);/*Escreve planilha em arquivo*/
 		saida.flush();
 		saida.close();
 		
 		System.out.println("Planilha foi criada");
+
 	}
 
 }
